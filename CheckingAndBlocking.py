197e8eeeac434be53d8746a5ccd5a25616ec90e6
Felix Adams <felixtadams1@gmail.com>
2017-11-07 11:01:55 -0500

    Corey and I added our first attempt at regular expression into CheckingAndBlocking.

--- CheckingAndBlocking.py
+++ CheckingAndBlocking.py
@@ -1,42 +1,45 @@
-#smoothPathFile = open('insert smooth path waypoint file name here', 'r')
-#rawPathFile = open('insert raw path waypoint file name here', 'r')
-#osbtaclesFile = open('insert obstacle date file name here'', 'r')
-adjustmentsFile = open('adjustments.txt', 'w')
+import re
+
+smoothPathFile = open('smooth_path.txt', 'r')
+rawPathFile = open('raw_path.txt', 'r')
+newFlightInformationFile = open('flight_information_new.txt', 'w')
+flightInformationFile = open('flight_information.txt', 'r')
 
 collisions = []
 
+coordSearch = re.compile('\w\b\.\w*')
+tagSearch = re.compile('^[a-z]*')
+
 #Check to see if any smooth path waypoint is in an obstacle
 for waypoint in smoothPathFile:
-    for obstacle in obstaclesFile:
-        if obstacle == static:
-            if abs(waypoint.x - obstacle.x) < obstacle.radius:
+    for obstacle in flightInformationFile:
+        if re.finall(obstacle, tagSearch)[0] == 'static':
+            if sqrt((re.findall(waypoint, coordsearch)[0] - re.finall(coordsearch, obstacle)[0])**2 + (re.findall(coordsearch, waypoint)[1] - re.finall(coordsearch, obstacle)[1])**2) < re.findall(coordsearch, obstacle)[4] and re.findall(coordsearch, waypoint)[3] < re.findall(coordsearch, obstacle)[3]:
                 collisions.append(obstacle)
-            if abs(waypoint.y - obstacle.y) < obstacle.radius:
-                collisions.append(obstacle)
-        if obstacle == dynamic:
-            if 'distance from line connecting the two waypoins' < obstacle.radius:
+        if re.findall(tagSearch, obstacle)[0] == 'dynamic':
+            if 'distance from line connecting the two waypoins' < re.findall(coordSearch, obstacle)[7]:
                 collisions.append(obstacle)
 
 #Check to see if the line connecting smooth path waypoints is too close to obstacle
 for waypoint in smoothPathFile:
     for obstacle in obstaclesFile:
-        if obstacle == static:
+        if re.finall(obstacle, tagSearch)[0] == 'static':
             if '''distance from line connecting waypoint and waypoint +1 and vertical
-            line at (obstacle.x, obstacle.y)''' < obstacle.radius:
+            line at (obstacle.x, obstacle.y)''' < re.findall(coordSearch, obstacle)[4]:
                 collisions.append(obstacle)
-        if obstacle == dynamic:
+        if re.findall(tagSearch, obstacle)[0] == 'dynamic':
             if '''distance from line connecting waypoint and waypoint +1 and line
-            connecting the two obstacle waypoints''' < obstacle.radius:
+            connecting the two obstacle waypoints''' < re.findall(coordSearch, obstacle)[7]:
                 collisions.append(obstacle)
 
 if len(collisions) == 0:
-    adjustmentsFile.write('We\'re A okay, boss')
+    newFlightInformationFile.write('We\'re A okay, boss')
 
-else if len(collisions) > 0:
+elif len(collisions) > 0:
     for obstacle in collisions:
         if obstacle == static:
-            adjustments.write('Insert new obstacle data here')                      #Possibly add an extra method to 'obstacle' class that returns its data as a tuple
+            newFlightInformationFile.write('Insert new obstacle data here')
         if obstacle == dynamic:
-            adjustments.write('Insert new obstacle data here')
+            newFlightInformationFile.write('Insert new obstacle data here')
 
 
