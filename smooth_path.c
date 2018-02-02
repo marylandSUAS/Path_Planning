803873ce4998c54b122223251eb94d6f7b5e2284
ThatGuyNamedTim <tim.j.thoresen@gmail.com>
2018-02-02 17:12:52 -0500

    Rough draft, must test for memory leaks and proper output

--- smooth_path.c
+++ smooth_path.c
@@ -16,6 +16,8 @@
 #define C_2 (.579796) // Exact: (2/5) * (sqrt(6) - 1)
 #define C_3 (.346) // Exact: (C_2 + 4)/(C_1 + 6));
 #define C_4 (1.12259)
+#define MAX(x,y) (((x) > (y)) ? (x): (y))
+#define MIN(x,y) (((x) < (y)) ? (x): (y))
 
 /* Structure for a 2D point and 3-D vector, the calcations will be done for 3-D waypoints
  * by projecting the three 3-D waypoints (a Vector) onto a plane (to become a Point) and doing the
@@ -46,26 +48,25 @@ Vector sub_vectors(Vector* a, Vector* b);
 Vector scale_vector(float scale, Vector* a);
 float norm_vector(Vector* a);
 Vector unit_vector(Vector* a, Vector* b);
+Vector* cross_prod_vector(Vector* a, Vector* b);
 
 Point add_points(Point* a, Point* b);
 Point sub_points(Point* a, Point* b);
 Point* scale_point(float scale, Point* a);
-float norm_pointm(Point* a);
+float norm_point(Point* a);
+float dist_point(Point* a, Point* b);
 float angle_point(Point* a, Point* b);
 float dot_product_point(Point* a, Point* b);
 Point unit_point(Point* a, Point* b);
 
-void bezier_curve_smooth(Point* a, Point* b, Point* c);
+float** bezier_curve_smooth(Point* a, Point* b, Point* c);
 void smooth(Vector* a, Vector* b, Vector* c);
 void print_waypoint(Vector* a);
 
 // Matrix operations needed to convert from 3-D waypoints to 2-D points and vice-versa
-float** inverse(float mat[4][4]);
+float** inverse(float** mat);
 float determinant(float mat[3][3]);
-
-float** mult_mat(int row1, int share_dim ,int col2, float mat1[row1][share_dim],
-                                                    float mat2[share_dim][col2]);
-
+float** mult_mat(int row1, int share_dim ,int col2, float** mat1, float** mat2);
 
 //////////////////////////////////////////////////////////////////////////////////////
 /// BASIC OPERATIONS NECESSARY TO COMPLETE MATH AS IN PAPER //////////////////////////
@@ -115,6 +116,15 @@ Vector unit_vector(Vector* a, Vector* b) {
     ans.z /= mag;
 }
 
+// Cross product of two vectors (a x b)
+Vector* cross_prod_vector(Vector* a, Vector* b) {
+  Vector* ans = malloc(sizeof(Vector));
+  ans->x = (a->y * b->z) - (a->z * b->y);
+  ans->y = (a->z * b->x) - (a->x * b->z);
+  ans->z = (a->x * b->y) - (a->y * b->x);
+  return ans;
+}
+
 // Add two 2-D points, note the second argument is assumed to be dynamically allocated
 Point add_points(Point* a, Point* b) {
   Point ans;
@@ -137,7 +147,7 @@ Point sub_points(Point* a, Point* b) {
  * a dynamically allocated point passed directly to another function (add/sub)
  */
 Point* scale_point(float scale, Point* a) {
-  Point* ans = malloc(sizeof(Point *));
+  Point* ans = malloc(sizeof(Point));
   ans->x = a->x * scale;
   ans->y = a->y * scale;
   return ans;
@@ -148,6 +158,13 @@ float norm_point(Point* a) {
   return sqrt(pow(a->x,2) + pow(a->y,2));
 }
 
+// Distance between two points
+float dist_point(Point* a, Point* b) {
+  Point ans;
+  ans.x = a->x - b->x;
+  ans.y = a->y - b->y;
+  return norm_point(&ans);
+}
 // Find the angle between two Points
 float angle_point(Point* a, Point* b) {
   return acos((dot_product_point(a,b))/(norm_point(a) * norm_point(b)));
@@ -172,11 +189,11 @@ Point unit_point(Point* a, Point* b) {
  * methodology explained here (for 3x3):
  *          http://www.ccodechamp.com/c-program-to-find-inverse-of-matrix/
  */
-float** inverse(float mat[4][4]) {
+float** inverse(float** mat) {
     // Allocate the return matrix
-    float** ans = (float **)malloc(4 * 4 * sizeof(float *));
+    float** ans = (float **)malloc(4 * sizeof(float *));
     for (int i = 0; i < 4; i++) {
-        ans[i] = (float *)malloc(4 * sizeof(float *));
+        ans[i] = malloc(4 * sizeof(float));
     }
 
     // Find the cofactor and also find the determinant
@@ -231,14 +248,12 @@ float determinant(float mat[3][3]){
 }
 
 // Multiply two matrix of any dimensions
-float** mult_mat(int row1, int share_dim ,int col2, float mat1[row1][share_dim],
-                                                    float mat2[share_dim][col2]) {
-
+float** mult_mat(int row1, int share_dim ,int col2, float** mat1, float** mat2) {
   float** ans;
   // dynamically allocate the answer (THIS MUST BE FREED)
-  ans = malloc(sizeof(float *) * row1);
+  ans = (float **)malloc(row1 * sizeof(float *));
   for (int i = 0; i < row1; i++) {
-    ans[i] = malloc(sizeof(float) * col2);
+    ans[i] = (float *)malloc(col2 * sizeof(float));
   }
 
    // Loop through all dimensions of output matrix
@@ -261,8 +276,122 @@ float** mult_mat(int row1, int share_dim ,int col2, float mat1[row1][share_dim],
 /* This will take in three vectors and convert to 2-D, do the bezier curve
  * calculations, convert back to 3-D and then print to output file
  */
-void smooth(Vector* a, Vector* b, Vector* c) {
-    // Calculate variables necessary to convert between 2-D and 3-D
+void smooth(Vector* w_1, Vector* w_2, Vector* w_3) {
+  // Calculate unit vectors necessary to convert between 2-D and 3-D
+  Vector u_t = unit_vector(w_1, w_2);
+  Vector u_p = unit_vector(w_3, w_2);
+  Vector* u_b = cross_prod_vector(&u_t,&u_p);
+  Vector* u_n = cross_prod_vector(u_b, &u_t);
+
+  // Construct transformation matrix
+  float** tm = (float **)malloc(4 * sizeof(float *));
+  for (int i = 0; i < 4; i++) {
+    tm[i] = (float *)malloc(4 * sizeof(float));
+  }
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (i == 3) {
+        tm[i][j] = (i == j) ? 1 : 0;
+      } else if (j == 0) {
+        switch(i) {
+          case 0: tm[i][j] = u_t.x; break;
+          case 1: tm[i][j] = u_t.y; break;
+          default: tm[i][j] = u_t.z;
+        }
+      } else if (j == 1) {
+        switch(i) {
+          case 0: tm[i][j] = u_n->x; break;
+          case 1: tm[i][j] = u_n->y; break;
+          default: tm[i][j] = u_n->z;
+        }
+      } else if (j == 2) {
+        switch(i) {
+          case 0: tm[i][j] = u_b->x; break;
+          case 1: tm[i][j] = u_b->y; break;
+          default: tm[i][j] = u_b->z;
+        }
+      } else {
+        switch(i) {
+          case 0: tm[i][j] = w_1->x; break;
+          case 1: tm[i][j] = w_1->y; break;
+          default: tm[i][j] = w_1->z;
+        }
+      }
+    }
+  }
+
+  // These are dunamicallt created due to calling the cross product function
+  free(u_b);
+  free(u_n);
+
+  // 4 x 4 matrix
+  float** inv_tm = inverse(tm);
+
+
+  // Must make the waypoints have an extra 0 to allow transformation
+  float** w_1_4;
+  float** w_2_4;
+  float** w_3_4;
+  w_1_4[0][1] = w_1->x;
+  w_2_4[0][1] = w_2->x;
+  w_3_4[0][1] = w_3->x;
+  w_1_4[1][1] = w_1->y;
+  w_2_4[1][1] = w_2->y;
+  w_3_4[1][1] = w_3->y;
+  w_1_4[2][1] = w_1->z;
+  w_2_4[2][1] = w_2->z;
+  w_3_4[2][1] = w_3->z;
+  w_1_4[3][1] = w_2_4[3][1] = w_3_4[3][1] = 0;
+
+  // Convert to 2-D, all are 4 x 1 matrices
+  float** p_1_mat = mult_mat(4, 4, 1, inv_tm, w_1_4);
+  float** p_2_mat = mult_mat(4, 4, 1, inv_tm, w_2_4);
+  float** p_3_mat = mult_mat(4, 4, 1, inv_tm, w_3_4);
+
+  // inv_tm is dynamically allocated due to calling the inverse function
+  for (int i = 0; i < 4; i++) {
+    free(inv_tm[i]);
+  }
+  free(inv_tm);
+
+  // Obtain the (x,y) from the previously calculated matrices (p_1,p_2.p_3)
+  Point p_1 = {p_1_mat[0][0],p_1_mat[1][0]};
+  Point p_2 = {p_2_mat[0][0],p_2_mat[1][0]};
+  Point p_3 = {p_3_mat[0][0],p_3_mat[1][0]};
+  // The return value from mult_mat is dynamically allocated
+  for (int i = 0; i < 4; i ++) {
+    free(p_1_mat[i]);
+    free(p_2_mat[i]);
+    free(p_3_mat[i]);
+  }
+  free(p_1_mat);
+  free(p_2_mat);
+  free(p_3_mat);
+
+  // Run the 2-D bezier_curve_smooth on the converted waypoints
+  float** points = bezier_curve_smooth(&p_1, &p_2, &p_3);
+
+  // Convert the 2-D returned poinnts back to 3-D
+  float** output = mult_mat(4, 4, 8, tm, points);
+
+  // Print the points
+  FILE* data_writes = fopen("smooth_path.txt","a");
+  fprintf(data_writes, "%f %f %f\n", output[0][0], output[1][0], output[2][0]); //b_0
+  fprintf(data_writes, "%f %f %f\n", output[0][1], output[1][1], output[2][1]); //b_1
+  fprintf(data_writes, "%f %f %f\n", output[0][3], output[1][3], output[2][3]); //b_3
+  fprintf(data_writes, "%f %f %f\n", output[0][6], output[1][6], output[2][6]); //e_1
+  fprintf(data_writes, "%f %f %f\n", output[0][7], output[1][7], output[2][7]); //e_0
+  fclose(data_writes);
+
+  // The bezier_curve_smooth and mult_mat returns a dynamically allocated array
+  for (int i = 0; i < 4; i++) {
+    free(points[i]);
+    free(output[i]);
+  }
+  free(output);
+  free(points);
+
+
 
 
 }
@@ -270,7 +399,7 @@ void smooth(Vector* a, Vector* b, Vector* c) {
 /* Calucate the curve and print the five points to the output file that parameterize
  * the curve
  */
-void bezier_curve_smooth(Point* w_1, Point* w_2, Point* w_3) {
+float** bezier_curve_smooth(Point* w_1, Point* w_2, Point* w_3) {
 
   /* B and E will be points on the two bezier curves and u_? will be unit vectors
    * that will be used in the calculaiton of the bezier curve, which is stored
@@ -284,6 +413,8 @@ void bezier_curve_smooth(Point* w_1, Point* w_2, Point* w_3) {
   float gamma = M_PI - angle_point(&u_1, &u_2);
   float beta = gamma / 2;
   float d = C_4 * sin(beta) / (K_MAX * pow(cos(beta),2));
+  // Incase the curve in not possible
+  d = MIN(d , .9 * MIN(dist_point(w_1,w_2), dist_point(w_3,w_2)));
   float h = C_3 * d;
   float g = C_2 * C_3 * d;
   float k = ((6 * C_3 * cos(beta))/(C_2 + 4)) * d;
@@ -305,6 +436,45 @@ void bezier_curve_smooth(Point* w_1, Point* w_2, Point* w_3) {
   b_3 = add_points(&b_2,scale_point(k,&u_d));
   e_3 = sub_points(&e_2,scale_point(k,&u_d));
 
+  float** s_2D = (float **)malloc(4 * sizeof(float *));
+  for (int i = 0; i < 4; i++) {
+    s_2D[i] = (float *)malloc(8 * sizeof(float));
+  }
+
+  // Create a return matrix that has all the struc information
+  for (int row = 0; row < 4; row++) {
+    for (int col = 0; col < 8; col++) {
+      if (row == 3) {
+        s_2D[row][col] = 0;
+      } else if (row == 4) {
+        s_2D[row][col] = 1;
+      } else {
+        switch (row) {
+          case (1) :
+            s_2D[row][0] = b_0.x;
+            s_2D[row][1] = b_1.x;
+            s_2D[row][2] = b_2.x;
+            s_2D[row][3] = b_3.x;
+            s_2D[row][4] = e_0.x;
+            s_2D[row][5] = e_1.x;
+            s_2D[row][6] = e_2.x;
+            s_2D[row][7] = e_3.x;
+            break;
+          case (2) :
+            s_2D[row][0] = b_0.y;
+            s_2D[row][1] = b_1.y;
+            s_2D[row][2] = b_2.y;
+            s_2D[row][3] = b_3.y;
+            s_2D[row][4] = e_0.y;
+            s_2D[row][5] = e_1.y;
+            s_2D[row][6] = e_2.y;
+            s_2D[row][7] = e_3.y;
+            break;
+        }
+      }
+    }
+  }
+  return s_2D;
 }
 
 // Print a point to the output file of the smoothed path (smooth_path.txt)
@@ -340,7 +510,6 @@ int main(void) {
   // Calculations for smoothing three waypoints
   smooth(&(waypoints_in[0]), &(waypoints_in[1]), &(waypoints_in[2]));
 
-
   // Read in one line and shift the array so that it is three points
   while (feof(data_read) == 0) {
     // Shift array
@@ -353,11 +522,8 @@ int main(void) {
 
     // Calculations for smoothing three waypoints
     smooth(&(waypoints_in[0]), &(waypoints_in[1]), &(waypoints_in[2]));
-
-
   }
 
   // Print ending point (finish point), no calulations necessary
   print_waypoint(&waypoints_in[2]);
-
 }
