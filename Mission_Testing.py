2df2b140882afc12d4cffd425bb839e57bb13e6b
derekbt96 <derekbt@yahoo.com>
2018-02-02 17:50:33 -0500

    Clean Up

--- Mission_Testing.py
+++ Mission_Testing.py
@@ -35,7 +35,19 @@ def Mission(mission):
 		pass
 
 	if (mission == 'Real1'):
-		pass
+		# dropspot1 = Locationwp().Set(38.3652711,-76.5366065,50, 16)
+
+		# bounds.append(Locationwp().Set(38.3652015,-76.5390927,0, 16))
+		# bounds.append(Locationwp().Set(38.3659207,-76.5388942,0, 16))
+		# bounds.append(Locationwp().Set(38.3658198,-76.5372419,0, 16))
+		# bounds.append(Locationwp().Set(38.3669806,-76.5368342,0, 16))
+		# bounds.append(Locationwp().Set(38.3666441,-76.5348816,0, 16))
+		# bounds.append(Locationwp().Set(38.3659375,-76.5350318,0, 16))
+		# bounds.append(Locationwp().Set(38.3652309,-76.5335298,0, 16))
+		# bounds.append(Locationwp().Set(38.3647766,-76.5338302,0, 16))
+
+		# offaxisloc = Locationwp().Set(38.3646505,-76.5376335,0, 16)
+
 
 	if (mission == 'Real2'):
 		pass
